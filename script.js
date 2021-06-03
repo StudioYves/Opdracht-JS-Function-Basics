23740e5c26ec6956deacd84154f6d3ac751dd131
Ivo Kersten <82444976+StudioYves@users.noreply.github.com>
2021-06-03 16:59:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Let's make pizza and sushi!")
+
+//define the function to make a pizza
+
+const makePizza = function() {
+    console.log("Flatten the dough");
+    console.log("spread tomato sauce onto the dough");
+    console.log("put salami on top of it");
+    console.log("put champignons on it");
+    console.log("Cover the pizza all-over with cheese");
+    console.log("bake the pizza 10 minutes in the over at 220 degrees");
+    console.log("--------------------")
+};
+
+//call the function to make a pizza
+//makePizza();
+//makePizza();
+//makePizza();
+
+//define the function to make sushi
+const makeSushi = function() {
+    console.log("Boil rice");
+    console.log("Chop the salmon");
+    console.log("Put the seaweed leaves on the table");
+    console.log("Put some rice and salomon on it");
+    console.log("Roll it all together");
+    console.log("Chop the long roll into little parts");
+    console.log("--------------------")
+};
+
+//call the function to make sushi
+//makeSushi();
+
+
+//We got an order for 3x Sushi and 2x Pizza
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
+
+
